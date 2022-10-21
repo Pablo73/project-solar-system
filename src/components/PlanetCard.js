1a18809452c43c280f5592e5c7a8a367ddf796dd
Pablo Dominguez <pasaxo@gmail.com>
2022-10-21 17:37:14 -0300

    Requistos 5, 6

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName } = this.props;
+    const { planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">
+          { planetName }
+        </p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.shape({
+    name: PropTypes.string,
+  }).isRequired,
+};
+
+PlanetCard.propTypes = {
+  planetImage: PropTypes.shape({
+    image: PropTypes.string,
+  }).isRequired,
+};
+
+export default PlanetCard;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <ul>
+          { planets.map((planet, index) => (
+            <PlanetCard
+              key={ `${planet.name}=${index}` }
+              planetName={ planet.name }
+              planetImage={ planet.image }
+            />))}
+        </ul>
       </div>
     );
   }
